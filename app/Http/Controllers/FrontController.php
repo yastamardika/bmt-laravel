57cffd763fd12c207d30961a605a809cfac94e2c
muklasr <muclasrachmanto@gmail.com>
2019-11-08 22:28:42 +0700

    add limit to eloquent query

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -9,7 +9,7 @@ class FrontController extends Controller
 {
     public function utama()
     {
-        $artist = DB::table('artist')->get();
+        $artist = DB::table('artist')->limit(6)->get();
 
         return view('pages.main', ['artist' => $artist]);
     }
