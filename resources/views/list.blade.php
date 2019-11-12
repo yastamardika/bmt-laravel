4edb59cdaa01596b94638c9ae1af83e43eee195e
muklasr <muclasrachmanto@gmail.com>
2019-11-12 13:42:35 +0700

    add blade foreach

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -3,24 +3,27 @@
 @section('content')
 <div class="" style="padding:10px">
     <h1>
-        category.name
+        {{ $kategori }}
     </h1>
-    <div class="container" style="">
+    <div class="container">
         <div class="row">
             <div class="col-12 col-sm-8 col-lg-5">
                 <h6 class="text-muted">List Group with Images</h6>
                 <ul class="list-group text-light">
-                    <li class="list-group-item d-flex justify-content-between align-items-center"
-                        style="background-color:black">
+
+                    @foreach($artist as $data)
+                    <li class="list-group-item d-flex justify-content-between align-items-center" style="background-color:black; margin-bottom:10px">
                         <div class="image-parent">
-                            <img src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/don_quixote.jpg"
-                                class="img-fluid" alt="quixote">
+                            <img src="{{ url('/data_file/'.$data->foto) }}" class="img-fluid" alt="quixote">
                         </div>
-                        Don Quixote
+                        {{ $data->nama_artist }}
                     </li>
+                    @endforeach
+
                 </ul>
             </div>
         </div>
+        {{ $artist->links() }}
     </div>
 </div>
 <br>
