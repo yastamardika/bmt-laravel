7479bbeda7e1be82242a66529fc820cf50b1a7ab
muklasr <muclasrachmanto@gmail.com>
2019-11-12 13:42:51 +0700

    add detail information

--- resources/views/pages/detail.blade.php
+++ resources/views/pages/detail.blade.php
@@ -1,34 +1,69 @@
 @extends('layouts.front')
 @section('konten')
-    <section id="speakers" class="wow fadeInUp" style="background-color:black">
+
+@foreach ($artist as $p)
+<section id="intro">
+    <div class="intro-container wow fadeIn">
+        <h1 class="mb-4 pb-0">{{ $p->nama_artist }}</h1>
+        <p class="mb-4 pb-0">Find the best art perfomance for you</p>
+        <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>
+        <a href="#speakers" class="about-btn scrollto">Discover More</a>
+    </div>
+</section>
+<section id="speakers" class="wow fadeInUp" style="background-color:black">
     <div class="container">
-        <div class="section-header">
-            <h2>Latest artist</h2>
-            <p>Here are some of our newest artist</p>
-        </div>
 
         <div class="row">
-            @foreach ($artist as $p)
-            <div class="col-lg-4 col-md-6">
-                <div class="speaker">
-                    <img src="{{ url('/data_file/'.$p->foto) }}" alt="Speaker 1" class="img-fluid">
-                    <div class="details">
-                        <h3><a href="/category/list/detail/{{ $p->id }}">{{ $p->nama_artist }}</a></h3>
-                        <p>Quas alias incidunt</p>
-                        <div class="social">
-                            <a href=""><i class="fa fa-twitter"></i></a>
-                            <a href=""><i class="fa fa-facebook"></i></a>
-                            <a href=""><i class="fa fa-google-plus"></i></a>
-                            <a href=""><i class="fa fa-linkedin"></i></a>
-                        </div>
+            <div class="col-lg-6 col-md-6">
+                {{ $p->deskripsi }}
+            </div>
+            <div class="col-lg-6 col-md-6">
+                <div class="row">
+                    <div class="col-lg-5 col-md-5">
+                        Nama
+                    </div>
+                    <div class="col-lg-1 col-md-1">
+                        :
+                    </div>
+                    <div class="col-lg-6 col-md-6"> {{ $p->nama_artist }}
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-5 col-md-5">
+                        Owner
+                    </div>
+                    <div class="col-lg-1 col-md-1">
+                        :
+                    </div>
+                    <div class="col-lg-6 col-md-6"> {{ $p->nama_owner }}
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-5 col-md-5">
+                        Nomor Telepon
+                    </div>
+                    <div class="col-lg-1 col-md-1">
+                        :
+                    </div>
+                    <div class="col-lg-6 col-md-6"> {{ $p->nomor_telepon }}
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-5 col-md-5">
+                        Alamat
+                    </div>
+                    <div class="col-lg-1 col-md-1">
+                        :
+                    </div>
+                    <div class="col-lg-6 col-md-6"> {{ $p->alamat }}
                     </div>
                 </div>
             </div>
-            @endforeach
 
         </div>
 
     </div>
 
 </section>
+@endforeach
 @endsection
