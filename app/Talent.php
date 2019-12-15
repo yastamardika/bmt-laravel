2813774e60ed9531222a31891868f969367c7fd5
ystmrdk <rattyasta1@gmail.com>
2019-12-15 15:07:43 +0700

    try tag

--- app/Talent.php
+++ app/Talent.php
@@ -1,11 +1,12 @@
 <?php
 
 namespace App;
-
+use \Spatie\Tags\HasTags;
 use Illuminate\Database\Eloquent\Model;
 
 class Talent extends Model
 {
+    use HasTags;
     protected $table = "artist";
 
     protected $fillable = ['nama_artist','kategori','nama_owner','deskripsi','nomor_telepon','alamat','foto'];
---
+++ database/migrations/2019_12_15_063415_add_taggables_to_artist.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTaggablesToArtist extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('artist', function (Blueprint $table) {
+            $table->morphs('taggable');
+
+            $table->foreign('id')->references('id')->on('tags')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('artist', function (Blueprint $table) {
+            $table->morphs('taggable');
+
+            $table->foreign('id')->references('id')->on('tags')->onDelete('cascade');
+        });
+    }
+}
